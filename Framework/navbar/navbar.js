55ced20f05ce397080af9e7dee951029a4d7ebe4
Haris Ahmad <me.haris2003@gmail.com>
2019-01-04 20:41:37 +0500

    Add sidepanel to navbar (finished)

---
+++ Framework/navbar/navbar.js
@@ -0,0 +1,5 @@
+/* Set the width of the side navigation to 250px */
+function toggleNav() {
+  document.getElementById("mySidenav").classList.toggle("width");
+  document.getElementById("main").classList.toggle("margin-left");
+}
